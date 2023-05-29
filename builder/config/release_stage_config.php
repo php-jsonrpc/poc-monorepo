8c47ccebc1fac85d92ac3add00cda1be399750fa
Yoanm <yoanm@users.noreply.github.com>
2023-05-29 14:10:48 +0200

    Fix config

--- builder/config/release_stage_config.php
+++ builder/config/release_stage_config.php
@@ -2,12 +2,9 @@
 declare(strict_types=1);
 
 use Symplify\MonorepoBuilder\Config\MBConfig;
-use PhpJsonRpc\Builder\ReleaseWorker as Worker;
 
 return static function (MBConfig $config): void {
     // Release workers in order to execute
     $config->workers([
-        Worker\Test4Worker::class,
-        Worker\Test2Worker::class,
     ]);
 };
