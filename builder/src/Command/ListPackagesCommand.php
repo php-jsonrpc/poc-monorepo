f62b28191b6c35ada703103507f5c80b788ea7a3
Yoanm <yoanm@users.noreply.github.com>
2023-06-03 21:11:17 +0200

    Improve

--- builder/src/Command/ListPackagesCommand.php
+++ builder/src/Command/ListPackagesCommand.php
@@ -27,23 +27,12 @@ class ListPackagesCommand extends AbstractSymplifyCommand
     {
         $this->setName('packages:list')
             ->setDescription('Output list of known packages')
-            ->addOption('all-fields', null, InputOption::VALUE_NONE, 'Display all available fields. Default to "field" option value.')
-            ->addOption('json', null, InputOption::VALUE_NONE, 'Output the list as JSON object')
-            ->addOption('field', null, InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, 'Fields to display, vendor, name and path by default', ['name', 'path'])
-            ->addOption('with-field', null, InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, 'List of fields to append to registered ones', []);
+            ->addOption('json', null, InputOption::VALUE_NONE, 'Output the list as JSON object');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $allField = $input->getOption('all-fields');
-        /** @var array<int, string> $displayFieldList */
-        $displayFieldList = true === $allField ? self::FIELD_LIST : $input->getOption('field');
-
-        $additionalDisplayFieldList = $input->getOption('with-field');
-        if (false === $allField && false === empty($additionalDisplayFieldList)) {
-            $displayFieldList = array_merge($displayFieldList, $additionalDisplayFieldList);
-        }
-
+        $displayFieldList = self::FIELD_LIST;
         $packageListInfos = [];
         foreach ($this->packageHelper->getPackages() as $package) {
             [$vendor, $shortName] = explode('/', $package->name);
@@ -51,10 +40,10 @@ class ListPackagesCommand extends AbstractSymplifyCommand
                 $displayFieldList,
                 function (array $carry, string $field) use ($package, $vendor, $shortName): array {
                     $carry[$field] = match ($field) {
-                        'vendor' => $vendor,
                         'name' => $package->name,
-                        'short_name' => $shortName,
                         'path' => $package->path,
+                        'vendor' => $vendor,
+                        'short_name' => $shortName,
                         default => throw new Exception(sprintf('Unknown field "%s"', $field))
                     };
 
